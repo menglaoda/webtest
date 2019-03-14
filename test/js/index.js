8d5346361a0918aa0c89f0f554185d9a31967ec8
menglaoda <496075284@qq.com>
2019-03-14 15:11:11 +0800

    html+css完成

--- test/js/index.js
+++ test/js/index.js
@@ -7,15 +7,61 @@ $(function(){
 	var score =60;
 	//错误答案
 	var erranswer = [];
+	//定时器
+	var timer  ;
+	//剩余时间
+	var mytime = 1800;
+	//控制时间
+	function time(){
+		mytime--;
+		//分
+		var m = parseInt(mytime/60);
+		//秒
+		var s = mytime%60;
+		if(mytime>1730){
+			$(".bttitle").fadeIn();
+		}else if(mytime<1730 &&mytime>600){
+			$(".bttitle").fadeOut();
+		}else if(mytime<300&&mytime>0){
+			$(".bttitle").fadeIn().css("color","red");
+		}else if(mytime<=0){
+			clearInterval(timer);
+			$("#btn").remove();
+			fn();	
+		}
+		$(".bttitle").html("剩余时间："+m+"分"+s+"秒");
+	}
+	//成绩
+	function fn(){
+		for(var i=0;i<$(".topic").length;i++){
+			submitanswer.push($(".topic").eq(i).attr("name"));
+		}
+		//计算成绩
+		for(var i=0;i<answer.length;i++){
+			if(answer[i]!=submitanswer[i]){
+				score-=2;
+				erranswer.push(i);
+			}
+		}
+		//成绩弹窗
+		$(".bttitle").hide();
+		$(".myscore").show();
+		$(".myscore span").html(score);
+		$(".zhezhao").show();
+	}
 	//给每个项目绑定事件
 	$(".myalert p").on("click",function(){
+		mytime=1800;
 		$(".myalert").hide();
 		$(".loading").show();
+		timer = setInterval(function(){
+			time();
+		},1000)
 		$(".headTitle").html($(this).html());
 		if($(this).html()=="html+css测试"){
 			$.ajax({
 				type:"get",
-				url:"js/index.json",
+				url:"js/htmlCss.json",
 				async:false,
 				success:function(res){
 					var num1 = num(res.length);//随机30道题目
@@ -40,9 +86,9 @@ $(function(){
 									}
 									var li1 = $("<li></li>");
 									var input1 = $("<input type='radio' />");
-									var span1 = $("<span></span>");
-									input1.attr("name",i).appendTo(li1);
-									span1.html('&ensp;'+s+"&ensp;"+item2).appendTo(li1);
+									var label1 = $("<label></label>");
+									input1.attr({"name":idx,"id":"like"+i+""+idx2}).appendTo(li1);
+									label1.attr("for","like"+i+""+idx2).html('&ensp;'+s+"&ensp;"+item2).appendTo(li1);
 									li1.appendTo(ol);
 								});
 								ol.appendTo(topic);
@@ -60,7 +106,7 @@ $(function(){
 					$(".zhezhao").hide();
 					$(".content").fadeIn();
 					$("body").css("overflow","visible");
-					//把答案记录在topic上
+					//点击选项把答案记录在topic上
 					$(".topic input").on("click",function(){
 						if($(this).parent().index()==0){
 							n="A";
@@ -77,7 +123,6 @@ $(function(){
 					})
 				}
 			});
-			console.log(answer);
 		}
 	})
 	//点击提交按钮
@@ -88,40 +133,35 @@ $(function(){
 			//没选中
 			if(typeof $(".topic").eq(i).attr("name")=="undefined"){
 				$(".topic").eq(i).css("border","crimson solid 1px");
-				$(".topic").eq(i).find(".bixuan").show();
-				$(".topic").eq(i).find("hr").show();
-				$(".bttitle").fadeIn();
+				$(".topic").eq(i).find(".bixuan").html("请选择选项").show();
+				$(".topic").eq(i).find("hr").show();			
 				an = false;
 			}
 		}
+		$(".unfinished").fadeIn().delay(5000).fadeOut();
 		//全选中
 		if(an==true){
+			$(".unfinished").hide();
 			$("#btn").remove();
-			console.log("全选中");
-			for(var i=0;i<$(".topic").length;i++){
-				submitanswer.push($(".topic").eq(i).attr("name"));
-			}
-			//计算成绩
-			for(var i=0;i<answer.length;i++){
-				if(answer[i]!=submitanswer[i]){
-					score-=2;
-					erranswer.push(i);
-				}
-			}
-			//成绩弹窗
-			$(".bttitle").hide();
-			$(".myscore").show();
-			$(".myscore span").html(score);
-			$(".zhezhao").show();
+			//console.log("全选中");
+			fn();
+			clearInterval(timer);
 		}
 	})
 	//查看错误题目
 	$(".errtimu").click(function(){
+		$(".bixuan").hide();
 		for(var i=0;i<erranswer.length;i++){
 			$(".topic").eq(erranswer[i]).css("background","gold");
+			$(".topic").eq(erranswer[i]).find("hr").show();
+			var nice = $("<p></p>");
+			nice.appendTo($(".topic").eq(erranswer[i]));
+			nice.html("正确答案为："+answer[erranswer[i]]).css({"color":"green","text-indent": "22px","margin":" 4px 0px"});
+			$(".topic").eq(erranswer[i]).css("border","solid 1px white");
 			$(".zhezhao").hide();
 			$(".myscore").addClass("errscore");
 			$(".myscore .errtimu").hide();
 		}
+		$(".topic input").off("click");
 	})
 })
--- test/js/num.js
+++ test/js/num.js
@@ -1,9 +1,10 @@
+//获取30个不一样的随机数
 function num(number){
 	var randoms=[];
 	while (true)
 	{
 	    var isExists = false;
-	    // 获取一个10–100范围的数
+	    // 获取一个小于number范围的数
 	    var random = parseInt(number* (Math.random()))
 	    // 判断当前随机数是否已经存在
 	    for (var i = 0; i < randoms.length; i++) {
@@ -15,7 +16,7 @@ function num(number){
 	    // 如果不存在，则添加进去
 	    if (!isExists)
 	        randoms.push(random);
-	    // 如果有10位随机数了，就跳出
+	    // 如果有30位随机数了，就跳出
 	    if (randoms.length === 30)
 	        break;
 	}
